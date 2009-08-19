8a0bba9bf79a421521ab16cf4b4f756f9a58b50e
FireFart <FireFart@fa1c2e9a-81d2-11de-92f0-6bbadbc12360>
2009-08-19 17:41:13 +0000

    Removed unused Imports
    git-svn-id: http://cyanogen-updater.googlecode.com/svn/trunk@195 fa1c2e9a-81d2-11de-92f0-6bbadbc12360

--- src/cmupdaterapp/utils/SysUtils.java
+++ src/cmupdaterapp/utils/SysUtils.java
@@ -3,12 +3,6 @@ package cmupdaterapp.utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.util.Log;
 
 public class SysUtils
