441039d17c8e71d3e87b44af9c8d5f8c08e3833d
FireFart <FireFart@fa1c2e9a-81d2-11de-92f0-6bbadbc12360>
2009-09-11 19:11:16 +0000

    Initial Work for multiple Theme Servers
    git-svn-id: http://cyanogen-updater.googlecode.com/svn/trunk@399 fa1c2e9a-81d2-11de-92f0-6bbadbc12360

--- src/cmupdaterapp/customTypes/ThemeList.java
+++ src/cmupdaterapp/customTypes/ThemeList.java
@@ -1,13 +1,58 @@
 package cmupdaterapp.customTypes;
 
 import java.io.Serializable;
+import java.util.LinkedList;
 
 import android.net.Uri;
+import android.util.Log;
 
-public class ThemeList  implements Serializable
+public class ThemeList implements Serializable
 {
 	private static final long serialVersionUID = 8861171977383611130L;
 	
 	public String name;
 	public Uri url;
+}
+
+class FullThemeList implements Serializable
+{
+	private static final long serialVersionUID = -2577705903002871714L;
+	
+	private static final String TAG = "<CM-Updater> FullThemeList";
+
+	private LinkedList<ThemeList> Themes;
+	
+	public FullThemeList()
+	{
+		Themes = new LinkedList<ThemeList>();
+	}
+	
+	public LinkedList<ThemeList> returnFullThemeList()
+	{
+		return Themes;
+	}
+	
+	public void addThemeToList(ThemeList t)
+	{
+		Themes.add(t);
+	}
+	
+	public boolean removeThemeFromList(ThemeList t)
+	{
+		try
+		{
+			Themes.remove(Themes.indexOf(t));
+			return true;
+		}
+		catch (Exception e)
+		{
+			Log.e(TAG, "Exception on Deleting Theme from List", e);
+			return false;
+		}
+	}
+	
+	public int getThemeCount()
+	{
+		return Themes.size();
+	}
 }
