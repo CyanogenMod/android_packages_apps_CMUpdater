4f027c060daf6d56d09c9c287c5fd4a426ce6109
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-08-15 03:00:00 +0100

    CMUpdater: Remove cruft

--- src/com/cyanogenmod/updater/ui/ApplyUpdateActivity.java
+++ src/com/cyanogenmod/updater/ui/ApplyUpdateActivity.java
@@ -2,6 +2,7 @@ package com.cyanogenmod.updater.ui;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
