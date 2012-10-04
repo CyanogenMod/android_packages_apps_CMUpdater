ee4ba3d9b346adbf5a6fde320f0b6dd0473c3311
DvTonder <david.vantonder@gmail.com>
2012-10-04 17:14:37 -0400

    CMUpdater: Fixes
    This commit includes:
     - Fix possible NPE in UpdateCheckService
     - Only do update check if network available
     - Add handling of connectivity changes
     - Fix Russian translation derp
     - Prevent AsyncTask from starting multiple times
     - Remove unused MD5Check task
    
    Change-Id: Ib66011d21c5865328634f53f0d51ce32723c0ad9

--- src/com/cyanogenmod/updater/service/UpdateCheckService.java
+++ src/com/cyanogenmod/updater/service/UpdateCheckService.java
@@ -19,6 +19,8 @@ import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.IBinder;
@@ -72,6 +74,8 @@ public class UpdateCheckService extends Service {
     private Integer mCurrentBuildDate;
     private boolean mShowNightlyRomUpdates;
     private boolean mShowAllRomUpdates;
+    private boolean mUpdateTaskRunning = false;
+    private AutoCheckForUpdatesTask mTask;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -89,29 +93,51 @@ public class UpdateCheckService extends Service {
 
     @Override
     public void onStart(Intent intent, int startId) {
-        // Parse the intent
-        boolean doCheck = intent.getBooleanExtra(Constants.CHECK_FOR_UPDATE, false);
-        if (doCheck) {
-            // If we should check for updates on start, do so in a seperate thread
-            new AutoCheckForUpdatesTask().execute();
+        // See if we have an intent to parse
+        if (intent != null) {
+            boolean doCheck = intent.getBooleanExtra(Constants.CHECK_FOR_UPDATE, false);
+            if (doCheck) {
+                // If we should check for updates on start, do so in a seperate thread
+                Log.i(TAG, "Checking for updates...");
+                if (!mUpdateTaskRunning) {
+                    mTask = new AutoCheckForUpdatesTask();
+                    mTask.execute();
+                }
+            }
         }
     }
 
     @Override
     public void onDestroy() {
         mCallbacks.kill();
+        if (mTask != null && mUpdateTaskRunning) {
+            mTask.cancel(true);
+            mTask = null;
+        }
         super.onDestroy();
     }
 
     //*********************************************************
     // Supporting methods and classes
     //*********************************************************
-    private class AutoCheckForUpdatesTask extends AsyncTask<Void, Void, Void> {
+    private class AutoCheckForUpdatesTask extends AsyncTask<Void, Void, Integer> {
+
         @Override
-        protected Void doInBackground(Void... params) {
-            checkForNewUpdates();
+        protected Integer doInBackground(Void... params) {
+            if (mUpdateTaskRunning) {
+                return null;
+            } else {
+                mUpdateTaskRunning = true;
+                checkForNewUpdates();
+            }
             return null;
         }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            mUpdateTaskRunning = false;
+        }
+
     }
 
     private final IUpdateCheckService.Stub mBinder = new IUpdateCheckService.Stub() {
@@ -133,7 +159,24 @@ public class UpdateCheckService extends Service {
         mToastHandler.sendMessage(mToastHandler.obtainMessage(0, ex));
     }
 
+    private boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+
     private void checkForNewUpdates() {
+        if (!isOnline()) {
+            // Only check for updates if the phone is actually connected to a network
+            Log.i(TAG, "Could not check for updates. Not connected to the network.");
+            return;
+        }
+
+        // Start the update check
+        mToastHandler.sendMessage(mToastHandler.obtainMessage(0, R.string.checking_for_updates, 0));
         FullUpdateInfo availableUpdates;
         while (true) {
             try {
@@ -150,13 +193,18 @@ public class UpdateCheckService extends Service {
             }
         }
 
-        // Store the last update check time
+        // Store the last update check time and ensure boot check completed is true
         Date d = new Date();
         SharedPreferences prefs = getSharedPreferences("CMUpdate", Context.MODE_MULTI_PROCESS);
         prefs.edit().putLong(Constants.LAST_UPDATE_CHECK_PREF, d.getTime()).apply();
+        prefs.edit().putBoolean(Constants.BOOT_CHECK_COMPLETED, true).apply();
 
         int updateCountRoms = availableUpdates.getRomCount();
         int updateCount = availableUpdates.getUpdateCount();
+
+        // Write to log
+        Log.i(TAG, "The update check successfully completed at " + d.toString() + " and found "
+                + updateCountRoms + " updates.");
 
         if (updateCountRoms == 0) {
             mToastHandler.sendMessage(mToastHandler.obtainMessage(0, R.string.no_updates_found, 0));
