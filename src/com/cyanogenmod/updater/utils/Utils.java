d44c5ff79ac8ae4b629c40ad2ad16343050e8d4c
Danny Baumann <dannybaumann@web.de>
2013-05-06 11:34:24 +0200

    Clean up CMUpdater.
    Remove a lot of legacy code (e.g. remainders of incremental ROM support)
    and get rid of weird design patterns, such as spawning another process
    to get a system property value.
    
    Also make the update check service a little more clever:
    - it's now able to be cancelled
    - it doesn't fetch changelogs that are later dropped anymore
    
    Change-Id: I07b16a0c3bb55791b0025698ede2c9e0f4e969de

---
+++ src/com/cyanogenmod/updater/utils/Utils.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * * Licensed under the GNU GPLv2 license
+ *
+ * The text of the license can be found in the LICENSE file
+ * or at https://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+package com.cyanogenmod.updater.utils;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.SystemProperties;
+import android.preference.PreferenceManager;
+
+import com.cyanogenmod.updater.misc.Constants;
+import com.cyanogenmod.updater.service.UpdateCheckService;
+
+public class Utils {
+    private Utils() {
+        // this class is not supposed to be instantiated
+    }
+
+    public static String getDeviceType() {
+        return SystemProperties.get("ro.cm.device");
+    }
+
+    public static String getInstalledVersion(boolean fullVersionString) {
+        String version = SystemProperties.get("ro.cm.version");
+        if (!fullVersionString) {
+            version = version.replaceAll("([0-9.]+?)-.+","$1");
+        }
+        return version;
+    }
+
+    public static long getInstalledBuildDate() {
+        return SystemProperties.getLong("ro.build.date.utc", 0);
+    }
+
+    public static String getUserAgentString(Context context) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            return pi.packageName + "/" + pi.versionName;
+        } catch (PackageManager.NameNotFoundException nnfe) {
+            return null;
+        }
+    }
+
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void scheduleUpdateService(Context context, int updateFrequency) {
+        // Load the required settings from preferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        long lastCheck = prefs.getLong(Constants.LAST_UPDATE_CHECK_PREF, 0);
+
+        // Get the intent ready
+        Intent i = new Intent(context, UpdateCheckService.class);
+        i.setAction(UpdateCheckService.ACTION_CHECK_SCHEDULED);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Clear any old alarms and schedule the new alarm
+        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        am.cancel(pi);
+
+        if (updateFrequency != Constants.UPDATE_FREQ_NONE) {
+            am.setRepeating(AlarmManager.RTC_WAKEUP, lastCheck + updateFrequency, updateFrequency, pi);
+        }
+    }
+}
