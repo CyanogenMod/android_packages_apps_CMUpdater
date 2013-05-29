942d06c846dcb76f2fdb38b7490587cfb9504947
Danny Baumann <dannybaumann@web.de>
2013-05-29 09:20:41 +0200

    Fix progress bar display when returning back to activity while download is running.
    Decouple UpdatePreference from UpdatesSettings in the process.
    
    Change-Id: Ib50486e3722fdf335b78e683d09154e955dc7c23

--- src/com/cyanogenmod/updater/UpdatesSettings.java
+++ src/com/cyanogenmod/updater/UpdatesSettings.java
@@ -66,7 +66,8 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.LinkedList;
 
-public class UpdatesSettings extends PreferenceActivity implements OnPreferenceChangeListener {
+public class UpdatesSettings extends PreferenceActivity implements
+        OnPreferenceChangeListener, UpdatePreference.OnReadyListener, UpdatePreference.OnActionListener {
     private static String TAG = "UpdatesSettings";
 
     // intent extras
@@ -166,9 +167,8 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
         final ActionBar bar = getActionBar();
         bar.setDisplayHomeAsUpEnabled(true);
 
-        // Turn on the Options Menu and update the layout
+        // Turn on the Options Menu
         invalidateOptionsMenu();
-        updateLayout();
     }
 
     @Override
@@ -227,6 +227,12 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
     }
 
     @Override
+    public void onReady(UpdatePreference pref) {
+        pref.setOnReadyListener(null);
+        mUpdateHandler.post(mUpdateProgress);
+    }
+
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mUpdateCheck) {
             int value = Integer.valueOf((String) newValue);
@@ -247,16 +253,15 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
 
     @Override
     protected void onStart() {
-        super.onResume();
+        super.onStart();
         updateLayout();
-        mUpdateHandler.post(mUpdateProgress);
         registerReceiver(mUpdateCheckFinishedReceiver,
                 new IntentFilter(UpdateCheckService.ACTION_CHECK_FINISHED));
     }
 
     @Override
     protected void onStop() {
-        super.onPause();
+        super.onStop();
         mUpdateHandler.removeCallbacks(mUpdateProgress);
         unregisterReceiver(mUpdateCheckFinishedReceiver);
         if (mProgressDialog != null) {
@@ -265,7 +270,8 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
         }
     }
 
-    /* package */ void startDownload(String key) {
+    @Override
+    public void onStartDownload(UpdatePreference pref) {
         // If there is no internet connection, display a message and return.
         if (!Utils.isOnline(this)) {
             Toast.makeText(this, R.string.data_connection_required, Toast.LENGTH_SHORT).show();
@@ -277,60 +283,60 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
             return;
         }
 
-        UpdatePreference pref = findMatchingPreference(key);
-        if (pref != null) {
-            // We have a match, get ready to trigger the download
-            mDownloadingPreference = pref;
-            UpdateInfo ui = mDownloadingPreference.getUpdateInfo();
-            if (ui != null) {
-                mDownloadingPreference.setStyle(UpdatePreference.STYLE_DOWNLOADING);
+        // We have a match, get ready to trigger the download
+        mDownloadingPreference = pref;
 
-                // Create the download request and set some basic parameters
-                String fullFolderPath = Environment.getExternalStorageDirectory().getAbsolutePath()
-                        + "/" + Constants.UPDATES_FOLDER;
-
-                // If directory doesn't exist, create it
-                File directory = new File(fullFolderPath);
-                if (!directory.exists()) {
-                    directory.mkdirs();
-                    Log.d(TAG, "UpdateFolder created");
-                }
-
-                // Save the Changelog content to the sdcard for later use
-                writeLogFile(ui.getFileName(), ui.getChangeLog());
-
-                // Build the name of the file to download, adding .partial at the end.  It will get
-                // stripped off when the download completes
-                String fullFilePath = "file://" + fullFolderPath + "/" + ui.getFileName() + ".partial";
-
-                Request request = new Request(Uri.parse(ui.getDownloadUrl()));
-                String userAgent = Utils.getUserAgentString(this);
-                if (userAgent != null) {
-                    request.addRequestHeader("User-Agent", userAgent);
-                }
-                request.addRequestHeader("Cache-Control", "no-cache");
-
-                request.setTitle(getString(R.string.app_name));
-                request.setDestinationUri(Uri.parse(fullFilePath));
-                request.setAllowedOverRoaming(false);
-                request.setVisibleInDownloadsUi(false);
-
-                // TODO: this could/should be made configurable
-                request.setAllowedOverMetered(true);
-
-                // Start the download
-                mDownloadId = mDownloadManager.enqueue(request);
-                mFileName = ui.getFileName();
-                mDownloading = true;
-
-                // Store in shared preferences
-                mPrefs.edit()
-                        .putLong(Constants.DOWNLOAD_ID, mDownloadId)
-                        .putString(Constants.DOWNLOAD_MD5, ui.getMD5Sum())
-                        .apply();
-                mUpdateHandler.post(mUpdateProgress);
-            }
+        UpdateInfo ui = mDownloadingPreference.getUpdateInfo();
+        if (ui == null) {
+            return;
         }
+
+        mDownloadingPreference.setStyle(UpdatePreference.STYLE_DOWNLOADING);
+
+        // Create the download request and set some basic parameters
+        String fullFolderPath = Environment.getExternalStorageDirectory().getAbsolutePath()
+                + "/" + Constants.UPDATES_FOLDER;
+
+        // If directory doesn't exist, create it
+        File directory = new File(fullFolderPath);
+        if (!directory.exists()) {
+            directory.mkdirs();
+            Log.d(TAG, "UpdateFolder created");
+        }
+
+        // Save the Changelog content to the sdcard for later use
+        writeLogFile(ui.getFileName(), ui.getChangeLog());
+
+        // Build the name of the file to download, adding .partial at the end.  It will get
+        // stripped off when the download completes
+        String fullFilePath = "file://" + fullFolderPath + "/" + ui.getFileName() + ".partial";
+
+        Request request = new Request(Uri.parse(ui.getDownloadUrl()));
+        String userAgent = Utils.getUserAgentString(this);
+        if (userAgent != null) {
+            request.addRequestHeader("User-Agent", userAgent);
+        }
+        request.addRequestHeader("Cache-Control", "no-cache");
+
+        request.setTitle(getString(R.string.app_name));
+        request.setDestinationUri(Uri.parse(fullFilePath));
+        request.setAllowedOverRoaming(false);
+        request.setVisibleInDownloadsUi(false);
+
+        // TODO: this could/should be made configurable
+        request.setAllowedOverMetered(true);
+
+        // Start the download
+        mDownloadId = mDownloadManager.enqueue(request);
+        mFileName = ui.getFileName();
+        mDownloading = true;
+
+        // Store in shared preferences
+        mPrefs.edit()
+                .putLong(Constants.DOWNLOAD_ID, mDownloadId)
+                .putString(Constants.DOWNLOAD_MD5, ui.getMD5Sum())
+                .apply();
+        mUpdateHandler.post(mUpdateProgress);
     }
 
     private Runnable mUpdateProgress = new Runnable() {
@@ -375,7 +381,8 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
         }
     };
 
-    /* package */ void stopDownload() {
+    @Override
+    public void onStopDownload(final UpdatePreference pref) {
         if (!mDownloading || mFileName == null) {
             return;
         }
@@ -387,10 +394,7 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         // Set the preference back to new style
-                        UpdatePreference pref = findMatchingPreference(mFileName);
-                        if (pref != null) {
-                            pref.setStyle(UpdatePreference.STYLE_NEW);
-                        }
+                        pref.setStyle(UpdatePreference.STYLE_NEW);
 
                         // We are OK to stop download, trigger it
                         mDownloadManager.remove(mDownloadId);
@@ -419,25 +423,11 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
         String fileName = new File(fullPathName).getName();
 
         // Find the matching preference so we can retrieve the UpdateInfo
-        UpdatePreference pref = findMatchingPreference(fileName);
+        UpdatePreference pref = (UpdatePreference) mUpdatesList.findPreference(fileName);
         if (pref != null) {
-            UpdateInfo ui = pref.getUpdateInfo();
             pref.setStyle(UpdatePreference.STYLE_DOWNLOADED);
-            startUpdate(ui);
+            onStartUpdate(pref);
         }
-    }
-
-    private UpdatePreference findMatchingPreference(String key) {
-        if (mUpdatesList != null) {
-            // Find the matching preference
-            for (int i = 0; i < mUpdatesList.getPreferenceCount(); i++) {
-                UpdatePreference pref = (UpdatePreference) mUpdatesList.getPreference(i);
-                if (pref.getKey().equals(key)) {
-                    return pref;
-                }
-            }
-        }
-        return null;
     }
 
     private String mapCheckValue(Integer value) {
@@ -591,12 +581,13 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
             }
 
             UpdatePreference up = new UpdatePreference(this, ui, style);
+            up.setOnActionListener(this);
             up.setKey(ui.getFileName());
 
             // If we have an in progress download, link the preference
             if (isDownloading) {
                 mDownloadingPreference = up;
-                mUpdateHandler.post(mUpdateProgress);
+                up.setOnReadyListener(this);
                 mDownloading = true;
             }
 
@@ -614,8 +605,9 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
         }
     }
 
-    public boolean deleteUpdate(String fileName) {
-        boolean success = false;
+    @Override
+    public void onDeleteUpdate(UpdatePreference pref) {
+        final String fileName = pref.getKey();
 
         if (mUpdateFolder.exists() && mUpdateFolder.isDirectory()) {
             File zipFileToDelete = new File(mUpdateFolder, fileName);
@@ -625,14 +617,12 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
                 zipFileToDelete.delete();
             } else {
                 Log.d(TAG, "Update to delete not found");
-                return false;
+                return;
             }
 
             if (logFileToDelete.exists()) {
                 logFileToDelete.delete();
             }
-
-            success = true;
 
             String message = getString(R.string.delete_single_update_success_message, fileName);
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
@@ -644,7 +634,6 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
 
         // Update the list
         updateLayout();
-        return success;
     }
 
     private void confirmDeleteAll() {
@@ -717,7 +706,10 @@ public class UpdatesSettings extends PreferenceActivity implements OnPreferenceC
         messageView.setTextAppearance(this, android.R.style.TextAppearance_DeviceDefault_Small);
     }
 
-    /* package */ void startUpdate(final UpdateInfo updateInfo) {
+    @Override
+    public void onStartUpdate(UpdatePreference pref) {
+        final UpdateInfo updateInfo = pref.getUpdateInfo();
+
         // Prevent the dialog from being triggered more than once
         if (mStartUpdateVisible) {
             return;
