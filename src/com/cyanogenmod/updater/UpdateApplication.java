4e0cca65d8e4cd07c23757bcef4dc799d24b64b3
Danny Baumann <dannybaumann@web.de>
2013-07-06 13:26:34 +0200

    Various UI and UX fixes.
    - Don't show notification if no updates are available.
    - Only notify of new updates (that is, newer than installed) instead of
      notifying of everything available
    - On download finish, show a notification instead of directly launching
      the activity and disrupting the user's workflow; allow installing the
      update from that notification
    - Handle cancel correctly (abort any pending HTTP requests)
    - Some cleanup
    
    Change-Id: I86411d11516fa1cca58a357d8feeb790d14bbbc6

---
+++ src/com/cyanogenmod/updater/UpdateApplication.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project (DvTonder)
+ *
+ * * Licensed under the GNU GPLv2 license
+ *
+ * The text of the license can be found in the LICENSE file
+ * or at https://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+package com.cyanogenmod.updater;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+public class UpdateApplication extends Application implements
+        Application.ActivityLifecycleCallbacks {
+
+    private boolean mMainActivityActive;
+
+    @Override
+    public void onCreate() {
+        mMainActivityActive = false;
+        registerActivityLifecycleCallbacks(this);
+    }
+
+    @Override
+    public void onActivityCreated (Activity activity, Bundle savedInstanceState) {
+    }
+
+    @Override
+    public void onActivityDestroyed (Activity activity) {
+    }
+
+    @Override
+    public void onActivityPaused (Activity activity) {
+    }
+
+    @Override
+    public void onActivityResumed (Activity activity) {
+    }
+
+    @Override
+    public void onActivitySaveInstanceState (Activity activity, Bundle outState) {
+    }
+
+    @Override
+    public void onActivityStarted (Activity activity) {
+        if (activity instanceof UpdatesSettings) {
+            mMainActivityActive = true;
+        }
+    }
+
+    @Override
+    public void onActivityStopped (Activity activity) {
+        if (activity instanceof UpdatesSettings) {
+            mMainActivityActive = false;
+        }
+    }
+
+    public boolean isMainActivityActive() {
+        return mMainActivityActive;
+    }
+}
--- src/com/cyanogenmod/updater/utils/Utils.java
+++ src/com/cyanogenmod/updater/utils/Utils.java
@@ -20,14 +20,21 @@ import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Environment;
+import android.os.PowerManager;
 import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.os.storage.StorageManager;
+import android.os.storage.StorageVolume;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import com.cyanogenmod.updater.R;
 import com.cyanogenmod.updater.misc.Constants;
 import com.cyanogenmod.updater.service.UpdateCheckService;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class Utils {
     private Utils() {
@@ -43,6 +50,7 @@ public class Utils {
         final NotificationManager nm =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         nm.cancel(R.string.not_new_updates_found_title);
+        nm.cancel(R.string.not_download_success);
     }
 
     public static String getDeviceType() {
@@ -87,7 +95,7 @@ public class Utils {
 
         // Get the intent ready
         Intent i = new Intent(context, UpdateCheckService.class);
-        i.setAction(UpdateCheckService.ACTION_CHECK_SCHEDULED);
+        i.setAction(UpdateCheckService.ACTION_CHECK);
         PendingIntent pi = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
 
         // Clear any old alarms and schedule the new alarm
@@ -97,5 +105,78 @@ public class Utils {
         if (updateFrequency != Constants.UPDATE_FREQ_NONE) {
             am.setRepeating(AlarmManager.RTC_WAKEUP, lastCheck + updateFrequency, updateFrequency, pi);
         }
+    }
+
+    public static void triggerUpdate(Context context, String updateFileName) throws IOException {
+        /*
+         * Should perform the following steps.
+         * 1.- mkdir -p /cache/recovery
+         * 2.- echo 'boot-recovery' > /cache/recovery/command
+         * 3.- if(mBackup) echo '--nandroid'  >> /cache/recovery/command
+         * 4.- echo '--update_package=SDCARD:update.zip' >> /cache/recovery/command
+         * 5.- reboot recovery
+         */
+
+        // Set the 'boot recovery' command
+        Process p = Runtime.getRuntime().exec("sh");
+        OutputStream os = p.getOutputStream();
+        os.write("mkdir -p /cache/recovery/\n".getBytes());
+        os.write("echo 'boot-recovery' >/cache/recovery/command\n".getBytes());
+
+        // See if backups are enabled and add the nandroid flag
+        /* TODO: add this back once we have a way of doing backups that is not recovery specific
+           if (mPrefs.getBoolean(Constants.BACKUP_PREF, true)) {
+           os.write("echo '--nandroid'  >> /cache/recovery/command\n".getBytes());
+           }
+           */
+
+        // Add the update folder/file name
+        // Emulated external storage moved to user-specific paths in 4.2
+        String userPath = Environment.isExternalStorageEmulated() ? ("/" + UserHandle.myUserId()) : "";
+
+        String cmd = "echo '--update_package=" + getStorageMountpoint(context) + userPath
+            + "/" + Constants.UPDATES_FOLDER + "/" + updateFileName
+            + "' >> /cache/recovery/command\n";
+        os.write(cmd.getBytes());
+        os.flush();
+
+        // Trigger the reboot
+        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        powerManager.reboot("recovery");
+    }
+
+    private static String getStorageMountpoint(Context context) {
+        StorageManager sm = (StorageManager) context.getSystemService(Context.STORAGE_SERVICE);
+        StorageVolume[] volumes = sm.getVolumeList();
+        String primaryStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        boolean alternateIsInternal = context.getResources().getBoolean(R.bool.alternateIsInternal);
+
+        if (volumes.length <= 1) {
+            // single storage, assume only /sdcard exists
+            return "/sdcard";
+        }
+
+        for (int i = 0; i < volumes.length; i++) {
+            StorageVolume v = volumes[i];
+            if (v.getPath().equals(primaryStoragePath)) {
+                /* This is the primary storage, where we stored the update file
+                 *
+                 * For CM10, a non-removable storage (partition or FUSE)
+                 * will always be primary. But we have older recoveries out there
+                 * in which /sdcard is the microSD, and the internal partition is
+                 * mounted at /emmc.
+                 *
+                 * At buildtime, we try to automagically guess from recovery.fstab
+                 * what's the recovery configuration for this device. If "/emmc"
+                 * exists, and the primary isn't removable, we assume it will be
+                 * mounted there.
+                 */
+                if (!v.isRemovable() && alternateIsInternal) {
+                    return "/emmc";
+                }
+            };
+        }
+        // Not found, assume non-alternate
+        return "/sdcard";
     }
 }
