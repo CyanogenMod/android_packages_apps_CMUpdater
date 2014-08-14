2fdb6b942a5263dfee4534fa4ed81d96cf8c1316
Adnan <adnan@cyngn.com>
2014-08-13 17:25:48 -0700

    CMUpdater: Handle download complete in background thread.
      - Since a broadcast receiver's timeout is ~10s, we should avoid doing
      too many operations, and instead offload to an intentservice.
    
    Change-Id: I713acfd078e3e81ed7405e027259429e7c3481ae

--- src/com/cyanogenmod/updater/receiver/DownloadReceiver.java
+++ src/com/cyanogenmod/updater/receiver/DownloadReceiver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The CyanogenMod Project
+ * Copyright (C) 2014 The CyanogenMod Project
  *
  * * Licensed under the GNU GPLv2 license
  *
@@ -10,27 +10,22 @@
 package com.cyanogenmod.updater.receiver;
 
 import android.app.DownloadManager;
-import android.app.DownloadManager.Query;
 import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.cyanogenmod.updater.R;
-import com.cyanogenmod.updater.UpdateApplication;
-import com.cyanogenmod.updater.UpdatesSettings;
 import com.cyanogenmod.updater.misc.Constants;
 import com.cyanogenmod.updater.misc.UpdateInfo;
+import com.cyanogenmod.updater.service.DownloadCompleteIntentService;
 import com.cyanogenmod.updater.service.DownloadService;
-import com.cyanogenmod.updater.utils.MD5;
 import com.cyanogenmod.updater.utils.Utils;
 
-import java.io.File;
 import java.io.IOException;
 
 public class DownloadReceiver extends BroadcastReceiver{
@@ -47,14 +42,14 @@ public class DownloadReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 
         if (ACTION_START_DOWNLOAD.equals(action)) {
             UpdateInfo ui = (UpdateInfo) intent.getParcelableExtra(EXTRA_UPDATE_INFO);
             handleStartDownload(context, ui);
         } else if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
             long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
-            handleDownloadComplete(context, prefs, id);
+            Log.d("Adnan", "action download intent " + intent.toString());
+            handleDownloadComplete(context, id);
         } else if (ACTION_INSTALL_UPDATE.equals(action)) {
             StatusBarManager sb = (StatusBarManager) context.getSystemService(Context.STATUS_BAR_SERVICE);
             sb.collapsePanels();
@@ -74,119 +69,23 @@ public class DownloadReceiver extends BroadcastReceiver{
         DownloadService.start(context, ui);
     }
 
-    private void handleDownloadComplete(Context context, SharedPreferences prefs, long id) {
+    private void handleDownloadComplete(Context context, long id) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         long enqueued = prefs.getLong(Constants.DOWNLOAD_ID, -1);
-        String incrementalFor = prefs.getString(Constants.DOWNLOAD_INCREMENTAL_FOR, null);
-
         if (enqueued < 0 || id < 0 || id != enqueued) {
             return;
         }
 
-        DownloadManager dm = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-
-        Intent updateIntent = new Intent(context, UpdatesSettings.class);
-        updateIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP |
-                Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-
-        int status = fetchDownloadStatus(context, id);
-        if (status == DownloadManager.STATUS_SUCCESSFUL) {
-            // Get the full path name of the downloaded file and the MD5
-
-            // Strip off the .partial at the end to get the completed file
-            String partialFileFullPath = fetchDownloadPartialPath(context, id);
-
-            if (partialFileFullPath == null) {
-                displayErrorResult(context, updateIntent, R.string.unable_to_download_file);
-            }
-
-            String completedFileFullPath = partialFileFullPath.replace(".partial", "");
-
-            File partialFile = new File(partialFileFullPath);
-            File updateFile = new File(completedFileFullPath);
-            partialFile.renameTo(updateFile);
-
-            String downloadedMD5 = prefs.getString(Constants.DOWNLOAD_MD5, "");
-
-            // Start the MD5 check of the downloaded file
-            if (MD5.checkMD5(downloadedMD5, updateFile)) {
-                // We passed. Bring the main app to the foreground and trigger download completed
-                updateIntent.putExtra(UpdatesSettings.EXTRA_FINISHED_DOWNLOAD_ID, id);
-                updateIntent.putExtra(UpdatesSettings.EXTRA_FINISHED_DOWNLOAD_PATH, completedFileFullPath);
-                updateIntent.putExtra(UpdatesSettings.EXTRA_FINISHED_DOWNLOAD_INCREMENTAL_FOR,
-                        incrementalFor);
-                displaySuccessResult(context, updateIntent, updateFile);
-            } else {
-                // We failed. Clear the file and reset everything
-                dm.remove(id);
-
-                if (updateFile.exists()) {
-                    updateFile.delete();
-                }
-                displayErrorResult(context, updateIntent, R.string.md5_verification_failed);
-            }
-        } else if (status == DownloadManager.STATUS_FAILED) {
-            // The download failed, reset
-            dm.remove(id);
-            displayErrorResult(context, updateIntent, R.string.unable_to_download_file);
-        }
-
+        // Send off to DownloadCompleteIntentService
+        Intent intent = new Intent(context, DownloadCompleteIntentService.class);
+        intent.putExtra(Constants.DOWNLOAD_ID, id);
+        context.startService(intent);
+        
         // Clear the shared prefs
         prefs.edit()
                 .remove(Constants.DOWNLOAD_MD5)
                 .remove(Constants.DOWNLOAD_ID)
                 .remove(Constants.DOWNLOAD_INCREMENTAL_FOR)
                 .apply();
-    }
-
-    private String fetchDownloadPartialPath(Context context, long id) {
-        Query query = new Query();
-        query.setFilterById(id);
-        Cursor c = ((DownloadManager)
-                context.getSystemService(Context.DOWNLOAD_SERVICE)).query(query);
-        try {
-            if (!c.moveToFirst()) {
-                c.close();
-                return null;
-            }
-
-            return c.getString(
-                    c.getColumnIndex(DownloadManager.COLUMN_LOCAL_FILENAME));
-        } finally {
-            c.close();
-        }
-    }
-
-    private int fetchDownloadStatus(Context context, long id) {
-        Query query = new Query();
-        query.setFilterById(id);
-        Cursor c = ((DownloadManager)
-                context.getSystemService(Context.DOWNLOAD_SERVICE)).query(query);
-        try {
-            if (!c.moveToFirst()) {
-                c.close();
-                return -1;
-            }
-            return c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
-        } finally {
-            c.close();
-        }
-    }
-
-    private void displayErrorResult(Context context, Intent updateIntent, int failureMessageResId) {
-        final UpdateApplication app = (UpdateApplication) context.getApplicationContext();
-        if (app.isMainActivityActive()) {
-            Toast.makeText(context, failureMessageResId, Toast.LENGTH_LONG).show();
-        } else {
-            DownloadNotifier.notifyDownloadError(context, updateIntent, failureMessageResId);
-        }
-    }
-
-    private void displaySuccessResult(Context context, Intent updateIntent, File updateFile) {
-        final UpdateApplication app = (UpdateApplication) context.getApplicationContext();
-        if (app.isMainActivityActive()) {
-            context.startActivity(updateIntent);
-        } else {
-            DownloadNotifier.notifyDownloadComplete(context, updateIntent, updateFile);
-        }
     }
 }
