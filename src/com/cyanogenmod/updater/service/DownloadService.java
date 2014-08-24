af27234e56cf22699cfc765cf5f3961440ce9808
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2014-08-24 17:45:02 +0200

    Small code-cleanup.
    This removes an unused method and cleans up duplicate code.
    
    Change-Id: Ic3be1cf49b391c6bfc89b59535802e633666fecb

--- src/com/cyanogenmod/updater/service/DownloadService.java
+++ src/com/cyanogenmod/updater/service/DownloadService.java
@@ -119,16 +119,6 @@ public class DownloadService extends IntentService
         return body;
     }
 
-    private UpdateInfo parseJSON(String json) {
-        try {
-            JSONObject obj = new JSONObject(json);
-            return jsonToInfo(obj);
-        } catch (JSONException e) {
-            Log.e(TAG, "JSONException", e);
-            return null;
-        }
-    }
-
     private UpdateInfo jsonToInfo(JSONObject obj) {
         try {
             if (obj == null || obj.has("errors")) {
@@ -170,19 +160,13 @@ public class DownloadService extends IntentService
 
     private void downloadIncremental() {
         Log.v(TAG, "Downloading incremental zip: " + mIncrementalUpdateInfo.getDownloadUrl());
-        // If directory doesn't exist, create it
-        File directory = Utils.makeUpdateFolder();
-        if (!directory.exists()) {
-            directory.mkdirs();
-            Log.d(TAG, "UpdateFolder created");
-        }
 
         // Build the name of the file to download, adding .partial at the end.  It will get
         // stripped off when the download completes
         String sourceIncremental = Utils.getIncremental();
         String targetIncremental = mFullUpdateInfo.getIncremental();
         String fileName = "incremental-" + sourceIncremental + "-" + targetIncremental + ".zip";
-        String incrementalFilePath = "file://" + directory.getAbsolutePath() + "/" + fileName + ".partial";
+        String incrementalFilePath = "file://" + getUpdateDirectory().getAbsolutePath() + "/" + fileName + ".partial";
 
         long downloadId = enqueueDownload(mIncrementalUpdateInfo.getDownloadUrl(), incrementalFilePath);
 
@@ -203,16 +187,9 @@ public class DownloadService extends IntentService
     private void downloadFullZip() {
         Log.v(TAG, "Downloading full zip");
 
-        // If directory doesn't exist, create it
-        File directory = Utils.makeUpdateFolder();
-        if (!directory.exists()) {
-            directory.mkdirs();
-            Log.d(TAG, "UpdateFolder created");
-        }
-
         // Build the name of the file to download, adding .partial at the end.  It will get
         // stripped off when the download completes
-        String fullFilePath = "file://" + directory.getAbsolutePath() +
+        String fullFilePath = "file://" + getUpdateDirectory().getAbsolutePath() +
                 "/" + mFullUpdateInfo.getFileName() + ".partial";
 
         long downloadId = enqueueDownload(mFullUpdateInfo.getDownloadUrl(), fullFilePath);
@@ -228,6 +205,17 @@ public class DownloadService extends IntentService
         Intent intent = new Intent(DownloadReceiver.ACTION_DOWNLOAD_STARTED);
         intent.putExtra(DownloadManager.EXTRA_DOWNLOAD_ID, downloadId);
         sendBroadcast(intent);
+    }
+
+    private File getUpdateDirectory() {
+        // If directory doesn't exist, create it
+        File directory = Utils.makeUpdateFolder();
+        if (!directory.exists()) {
+            directory.mkdirs();
+            Log.d(TAG, "UpdateFolder created");
+        }
+
+        return directory;
     }
 
     @Override
