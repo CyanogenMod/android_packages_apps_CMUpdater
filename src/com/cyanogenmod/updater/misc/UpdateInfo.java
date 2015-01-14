c0be4c3d8e367f7a018e15cbab2efe93bf04974f
Adnan Begovic <adnan@cyngn.com>
2015-01-14 14:16:36 -0800

    CMUpdater: Don't assume api_level won't be null from server.
    Change-Id: Id282e98b798eb5888653136f6a346d0100f8c25b

--- src/com/cyanogenmod/updater/misc/UpdateInfo.java
+++ src/com/cyanogenmod/updater/misc/UpdateInfo.java
@@ -25,8 +25,11 @@ public class UpdateInfo implements Parcelable, Serializable {
     private static final long serialVersionUID = 5499890003569313403L;
     private static final Pattern sIncrementalPattern =
             Pattern.compile("^incremental-(.*)-(.*).zip$");
+    private static final Pattern sCMVersionPattern =
+            Pattern.compile(".*?(\\d+)", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 
     public static final String CHANGELOG_EXTENSION = ".changelog.html";
+    public static final int INVALID_API_LEVEL = 0;
 
     public enum Type {
         UNKNOWN,
@@ -147,8 +150,12 @@ public class UpdateInfo implements Parcelable, Serializable {
             return mIsNewerThanInstalled;
         }
 
-        int installedApiLevel = Utils.getInstalledApiLevel();
-        if (installedApiLevel != mApiLevel && mApiLevel > 0) {
+        int installedApiLevel = Utils.getInstalledApiLevel();;
+        if (mApiLevel == INVALID_API_LEVEL) {
+            Matcher m = sCMVersionPattern.matcher(getFileName());
+            mIsNewerThanInstalled =  m.find()
+                    && Integer.parseInt(m.group(1)) > installedApiLevel;
+        } else if (installedApiLevel != mApiLevel && mApiLevel > 0) {
             mIsNewerThanInstalled = mApiLevel > installedApiLevel;
         } else {
             // API levels match, so compare build dates.
