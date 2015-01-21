946b7f581cf80a9586327ae8880e30b43c7d685c
Jacob Müller <jacob.mueller.elz@gmail.com>
2015-01-21 22:39:05 +0100

    CMUpdater: Drop DashClock extension and auto backup support
    * Drop DashClock extension support (No need for it in CM12 anymore)
    * Remove auto backup code (Has never workes properly)
    
    Change-Id: Ie30a4aabe3e115106b2c71cb294929ebc09e817f

--- Android.mk
+++ Android.mk
@@ -5,8 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_STATIC_JAVA_LIBRARIES := dashclockapi \
-                               volley
+LOCAL_STATIC_JAVA_LIBRARIES := volley
 
 LOCAL_PACKAGE_NAME := CMUpdater
 
@@ -15,10 +14,3 @@ LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard.flags
 LOCAL_PRIVILEGED_MODULE := true
 
 include $(BUILD_PACKAGE)
-
-##################################################
-include $(CLEAR_VARS)
-
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := dashclockapi:libs/dashclock-api-r1.1.jar
-
-include $(BUILD_MULTI_PREBUILT)
