1530dbef3289423fb9c8af8143f5ac4e770c10a9
dhacker29 <dhackerdvm@gmail.com>
2015-01-23 00:43:39 -0500

    CMUpdater: Use more current logic to determine install path
    The old logic was from before unified fstab. We no longer hard
    code the storage paths in fstab they are auto for emmc or else
    datamedia based.
    
    This fixes the issue where the path for datamedia devices was
    getting set to /sdcard/0/cmupdater/<filename>
    
    Without the path of /data upcypt will not work properly to allow
    updates on encrypted devices
    
    Change-Id: I5202faed2b8a1be916ddb55c079320c18ad1ab8c

--- Android.mk
+++ Android.mk
@@ -1,26 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
-ifneq ($(TARGET_RECOVERY_FSTAB),)
-  recovery_fstab := $(strip $(wildcard $(TARGET_RECOVERY_FSTAB)))
-else
-  recovery_fstab := $(strip $(wildcard $(TARGET_DEVICE_DIR)/recovery.fstab))
-endif
-
-ALTERNATE_IS_INTERNAL := false
-ifneq ($(recovery_fstab),)
-  recovery_fstab := $(ANDROID_BUILD_TOP)/$(recovery_fstab)
-  ifneq ($(shell grep "/emmc" $(recovery_fstab)),)
-  ALTERNATE_IS_INTERNAL := true
-  endif
-endif
-
 include $(CLEAR_VARS)
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-
-ifeq ($(ALTERNATE_IS_INTERNAL), true)
-  LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res-compat $(LOCAL_RESOURCE_DIR)
-endif
 
 LOCAL_MODULE_TAGS := optional
 
