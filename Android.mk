5337106664829673690b773534d84679e8f453c9
dhacker29 <dhackerdvm@gmail.com>
2015-04-26 18:29:08 -0400

    CMUpdater: Add a Tv specific theme and layout (1/2)
    Change-Id: I3c3d2907d8b0a1c41ca2a2a023fad7b6157fb1b8

--- Android.mk
+++ Android.mk
@@ -5,7 +5,18 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_STATIC_JAVA_LIBRARIES := volley
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v7-recyclerview \
+    android-support-v13 \
+    android-support-v17-leanback \
+    volley
+
+LOCAL_RESOURCE_DIR := \
+    $(TOP)/frameworks/support/v17/leanback/res \
+    $(LOCAL_PATH)/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay --extra-packages android.support.v17.leanback
 
 LOCAL_PACKAGE_NAME := CMUpdater
 
