2424b4d4f1886c3b81cad490e8bc922beccee60d
Adnan Begovic <adnan@cyngn.com>
2015-10-14 14:44:55 -0700

    CMUpdater: Temporary compile fixes.
    Change-Id: I20a009f35c53f681b6ab8a61b128066c11bcf380

--- src/com/cyanogenmod/updater/UpdatesSettings.java
+++ src/com/cyanogenmod/updater/UpdatesSettings.java
@@ -43,7 +43,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.internal.util.cm.ScreenType;
+/*import com.android.internal.util.cm.ScreenType;*/
 
 import com.cyanogenmod.updater.misc.Constants;
 import com.cyanogenmod.updater.misc.State;
@@ -234,9 +234,9 @@ public class UpdatesSettings extends PreferenceActivity implements
         super.onResume();
 
         // If running on a phone, remove padding around the listview
-        if (!ScreenType.isTablet(this)) {
+        /*if (!ScreenType.isTablet(this)) {
             getListView().setPadding(0, 0, 0, 0);
-        }
+        }*/
     }
 
     @Override
