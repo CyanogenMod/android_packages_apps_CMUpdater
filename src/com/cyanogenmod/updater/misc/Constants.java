6d555db600368ffdda42f079b696e6a8ea949f7e
Daniel Micay <danielmicay@gmail.com>
2016-01-07 12:21:32 -0500

    CMUpdater: Some squashes from CopperheadOS's fork.
    Rework CMUpdater to store OTA's in /data/ rather than on storage/sdcard.
    
    Yes, this will break people who wipe between updates.
    
    Some really old legacy issues explained on why we didn't do this before:
    * When CMUpdater was first written, we didn't really have many options as sdcard was the only thing that could take an ota.
    We're talking mtd days here when internal storage was measured in the hundreds of MB.
    * Even after that, there was encryption (and no uncrypt). Using /data/<app-path> wasn't viable on encrypted devices.
    
    Patches:
    
    make the download private
    
    6b1656c9d310f81bd76405e2493aea3a35273b30
    
    use a private directory for updates
    
    1771e9802603364832f6089a64dd1cbf3c22ebf9
    
    use DownloadManager's openDownloadedFile method
    
    6422039a13ae76caeb35970fd770ce28d9539493
    
    remove external storage permission
    
    4f8c2b24ab62f49235819d6f1bce2280e3e9ea74
    
    Change-Id: I8ec5534cec671e371ecbb44ecbca06fdc9d8aa43

--- src/com/cyanogenmod/updater/misc/Constants.java
+++ src/com/cyanogenmod/updater/misc/Constants.java
@@ -11,7 +11,7 @@ package com.cyanogenmod.updater.misc;
 
 public class Constants {
     // Download related
-    public static final String UPDATES_FOLDER = "cmupdater";
+    public static final String UPDATES_FOLDER = "updates";
     public static final String DOWNLOAD_ID = "download_id";
     public static final String DOWNLOAD_MD5 = "download_md5";
     public static final String DOWNLOAD_INCREMENTAL_FOR = "download_incremental_for";
