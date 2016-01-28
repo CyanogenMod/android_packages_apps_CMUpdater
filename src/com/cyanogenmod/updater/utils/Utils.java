dd91782690a0f29f451a08459c1d42daa344cd6e
Henning Nielsen Lund <hnl_dk@amigaos.dk>
2016-01-29 00:50:02 +0100

    CMUpdater: CleanUp
    Change the behavior to use android.os.RecoverySystem.installPackage() to uncrypt the package,
    reboot the system and trigger the recovery to install the package.
    Fixes auto-update for TWRP.
    
    RM-208
    
    Change-Id: Ifc31da8ca78fd4f0d4f833511ce35463c495fbcb
    Signed-off-by: Henning Nielsen Lund <hnl_dk@amigaos.dk>
    (cherry picked from commit 8456e7f8ccf552c237a61611ee984ec80dd148a3)

--- src/com/cyanogenmod/updater/utils/Utils.java
+++ src/com/cyanogenmod/updater/utils/Utils.java
@@ -43,7 +43,7 @@ public class Utils {
     }
 
     public static File makeUpdateFolder() {
-        return new File(Environment.getExternalStorageDirectory().getAbsolutePath(),
+        return new File(Environment.getExternalStorageDirectory(),
                 Constants.UPDATES_FOLDER);
     }
 
@@ -113,43 +113,16 @@ public class Utils {
     }
 
     public static void triggerUpdate(Context context, String updateFileName) throws IOException {
-        /*
-         * Should perform the following steps.
-         * 1.- mkdir -p /cache/recovery
-         * 2.- echo 'boot-recovery' > /cache/recovery/command
-         * 3.- if(mBackup) echo '--nandroid'  >> /cache/recovery/command
-         * 4.- echo '--update_package=SDCARD:update.zip' >> /cache/recovery/command
-         * 5.- reboot recovery
-         */
-
-        // Set the 'boot recovery' command
-        Process p = Runtime.getRuntime().exec("sh");
-        OutputStream os = p.getOutputStream();
-        os.write("mkdir -p /cache/recovery/\n".getBytes());
-        os.write("echo 'boot-recovery' >/cache/recovery/command\n".getBytes());
-
-        // See if backups are enabled and add the nandroid flag
-        /* TODO: add this back once we have a way of doing backups that is not recovery specific
-           if (mPrefs.getBoolean(Constants.BACKUP_PREF, true)) {
-           os.write("echo '--nandroid'  >> /cache/recovery/command\n".getBytes());
-           }
-           */
-
         // Add the update folder/file name
-        String primaryStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath();
-        // If data media rewrite the path to bypass the sd card fuse layer and trigger uncrypt
-        String directPath = Environment.maybeTranslateEmulatedPathToInternal(
-                new File(primaryStoragePath)).getAbsolutePath();
-        String updatePath = Environment.isExternalStorageEmulated() ? directPath :
-                primaryStoragePath;
-        String cmd = "echo '--update_package=" + updatePath + "/" + Constants.UPDATES_FOLDER + "/"
-                + updateFileName + "' >> /cache/recovery/command\n";
-        os.write(cmd.getBytes());
-        os.flush();
+        File primaryStorage = Environment.getExternalStorageDirectory();
+        // If the path is emulated, translate it, if not return the original path
+        String updatePath = Environment.maybeTranslateEmulatedPathToInternal(
+                primaryStorage).getAbsolutePath();
+        // Create the path for the update package
+        String updatePackagePath = updatePath + "/" + Constants.UPDATES_FOLDER + "/" + updateFileName;
 
-        // Trigger the reboot
-        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        powerManager.reboot("recovery");
+        // Reboot into recovery and trigger the update
+        android.os.RecoverySystem.installPackage(context, new File(updatePackagePath));
     }
 
     public static int getUpdateType() {
